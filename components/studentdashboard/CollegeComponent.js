7f06a3dc91935e2c554f881bde71635c112df3a5
manishnagar1000 <66521760+manishnagar1000@users.noreply.github.com>
2023-07-28 16:48:44 +0530

    loading in college part

--- components/studentdashboard/CollegeComponent.js
+++ components/studentdashboard/CollegeComponent.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
-
 import { Button, Badge, Card, Container, Row, Col, Nav } from "react-bootstrap";
 import Classes from "/styles/userdashboard.module.css";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
-import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
 import Link from "next/link";
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function CollegeComponent() {
   const [active, setActive] = useState(1);
   const [isRecommendationOpen, setIsRecommendationOpen] = useState(false);
   const [value, setValue] = React.useState("3");
   const [studentdata, setStudentdata] = React.useState([]);
+  const [isLoading, setIsLoading] = useState(true); // State to track loading status
 
   const handleTabClick = () => {
     setIsRecommendationOpen(!isRecommendationOpen);
@@ -35,8 +35,8 @@ export default function CollegeComponent() {
         }
     );
     const student = await student_res.json();
-    // console.log(student);
     setStudentdata(student.data);
+    setIsLoading(false); // Set loading to false after data fetch
   };
 
   useEffect(() => {
@@ -63,45 +63,48 @@ export default function CollegeComponent() {
       </Box>
 
       <div style={{ marginTop: "20px" }}>
-        {
-        studentdata.length>0?
-        studentdata?.map((s, i) => {
-          return (
-            <Card key={i} style={{ marginBottom: "20px", padding: "1rem" }}>
-              <Container>
-                <Row className="d-flex justify-content-center align-items-center">
-                  <Col md={2}>
-                    <Card.Img 
-                    className="img-fluid"
-                      src={s.logo_img_path}
-                      onError={(e) =>
-                        (e.target.src = "/assets/images/DummyLOGO.jpg")
-                      }
-                      alt="Image"
-                      style={{
-                        width: "80px",
-                        height: "80px",
-                        objectFit:"contain"
-                      }}
-                    />
-                  </Col>
-                  <Col md={7}>
-                    <h2>{s.name}</h2>
-                    <p>{s.short_address}</p>
-                  </Col>
-                  <Col md={3}>
-                    <Link href={`/colleges/${s.slug}`}>
-                      <Button variant="primary">View College</Button>
-                    </Link>
-                  </Col>
-                </Row>
-              </Container>
-            </Card>
-          );
-        })
-        :<div className="d-flex justify-content-center align-items-center fw-bold " style={{height:"76vh"}}>No College Data found</div>
-      
-        }
+        {isLoading ? (
+          <div className="d-flex justify-content-center align-items-center fw-bold " style={{ height: "76vh" }}>
+      <CircularProgress />
+          </div>
+        ) : studentdata.length > 0 ? (
+          studentdata?.map((s, i) => {
+            return (
+              <Card key={i} style={{ marginBottom: "20px", padding: "1rem" }}>
+                <Container>
+                  <Row className="d-flex justify-content-center align-items-center">
+                    <Col md={2}>
+                      <Card.Img 
+                        className="img-fluid"
+                        src={s.logo_img_path}
+                        onError={(e) =>
+                          (e.target.src = "/assets/images/DummyLOGO.jpg")
+                        }
+                        alt="Image"
+                        style={{
+                          width: "80px",
+                          height: "80px",
+                          objectFit:"contain"
+                        }}
+                      />
+                    </Col>
+                    <Col md={7}>
+                      <h2>{s.name}</h2>
+                      <p>{s.short_address}</p>
+                    </Col>
+                    <Col md={3}>
+                      <Link href={`/colleges/${s.slug}`}>
+                        <Button variant="primary">View College</Button>
+                      </Link>
+                    </Col>
+                  </Row>
+                </Container>
+              </Card>
+            );
+          })
+        ) : (
+          <div className="d-flex justify-content-center align-items-center fw-bold " style={{ height: "76vh" }}>No College Data found</div>
+        )}
       </div>
     </div>
   );
