b5eb8f0df174cb80b01ef72fb34e052b4de22eac
manishnagar1000 <66521760+manishnagar1000@users.noreply.github.com>
2024-02-16 13:54:39 +0530

    add gta tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,18 +14,40 @@ const structureddataOrg= {
     "areaServed": "IN",
     "availableLanguage": "en"
   },
+  
   "sameAs": [
     "https://www.facebook.com/learnerhunt/",
     "https://twitter.com/learnerhunt",
     "https://www.instagram.com/learnerhunt/",
     "https://www.youtube.com/channel/UCz4ZKdDFvEfRfXU68FFk8_g",
     "https://in.linkedin.com/company/learnerhunt-com"
-  ]
+  ],
+  
 }
 export default function Document() {
   return (
     <Html lang="en">
       <Head >
+      {/* Google Tag Manager */}
+      <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NG6GCR97');
+              `,
+            }}
+          />
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NG6GCR97" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
+          {/* End Google Tag Manager */}
       <script type="application/ld+json" dangerouslySetInnerHTML={{__html:JSON.stringify(structureddataOrg)}}/>
         </Head>
       <body>
